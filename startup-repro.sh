50937c732c3605c814909ce4c0db5b572039352b
Oren Ben-Meir <orenbm11@gmail.com>
2024-05-15 15:44:34 -0400

    fix postgres url

--- startup-repro.sh
+++ startup-repro.sh
@@ -1,4 +1,4 @@
-export POSTGRES_URL=localhost:5432
+export POSTGRES_URL=localhost:5432/flyway-test-db
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=memleak1
 nohup java -javaagent:./newrelic/newrelic.jar -jar ./app.jar >stdout.log &
